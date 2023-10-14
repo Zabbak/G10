4f6efbd034e59399376660aae4902b796f91dba5
Artur Souto <artursmpm@gmail.com>
2023-10-14 16:09:13 -0300

    Update para funcionar juntamento com django

--- projetosg10app/urls.py
+++ projetosg10app/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.paginainicial, name="paginainicial"),
     path("apadrinhamento", views.apadrinhamento, name="apadrinhamento"),
+    path("noticias", views.apadrinhamento, name="noticias"),
+    path("cadastroelogin", views.apadrinhamento, name="cadastroelogin"),
+    path("cadastro", views.apadrinhamento, name="cadastro"),
+    path("administrador", views.apadrinhamento, name="administrador"),
 ]
--- projetosg10app/views.py
+++ projetosg10app/views.py
@@ -7,11 +7,28 @@ def paginainicial(request):
     context = {
     }
     return render(request, 'paginicial.html', context)
+
 def noticias(request):
     context = {
     }
     return render(request, 'noticias.html', context)
+
 def apadrinhamento(request):
     context = {
     }
     return render(request, 'apadrinhamento.html', context)
+
+def cadastroelogin(request):
+    context = {
+    }
+    return render(request, 'cadastroelogin.html', context)
+
+def cadastro(request):
+    context = {
+    }
+    return render(request, 'cadastro.html', context)
+
+def administrador(request):
+    context = {
+    }
+    return render(request, 'administrador.html', context)
