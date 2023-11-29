9fade951b8e5bb5906139bad70fcc95a251cd011
Artur Souto <artursmpm@gmail.com>
2023-11-29 17:51:35 -0300

    StartTests

--- projetosg10app/tests.py
+++ projetosg10app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from selenium import webdriver
 
 # Create your tests here.
+class app_health(TestCase):
+
+    def setUp(self):
+        self.base_url = "http://localhost:8000"
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_argument("--no-sandbox")
+        options.add_argument('--headless')
+        options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(options=options)
+
+        self.driver.maximize_window()
+
+        self.driver.implicitly_wait(10)
